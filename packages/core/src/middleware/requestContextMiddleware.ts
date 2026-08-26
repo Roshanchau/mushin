0afed46efefcd7718cd186c074f156602fe36c88
Roshan Chaudhary <roshanchau001@gmail.com>
2026-08-26 13:24:24 +0545

    feat: adds request context middleware to show threadId in the attributes

---
+++ packages/core/src/middleware/requestContextMiddleware.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from "express";
+import { v7 as uuid } from "uuid";
+import { AsyncContextService } from "../services";
+
+export function requestContextMiddleware(req: Request, res: Response, next: NextFunction) {
+  AsyncContextService.runWithNewContext(() => {
+    AsyncContextService.setThreadId(uuid());
+    next();
+  });
+}
